702971141861067948a3bd5d906d845e2527572d
Giju Ambrose <giju@techversantinfo.com>
2020-08-11 18:46:58 +0530

    Added new table in analyse page

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -1,17 +1,19 @@
 class AnalyzeController < ApplicationController
 
   def index
-    @analyse_data = ArchivalNdsom.fetch_analyse_data
-    if params[:ajax_call].present? && params[:ajax_call] == "true" && params[:isis].present?
-       @analyse_data = @analyse_data.select { |x| x.isin.match?(params[:isis]) }
+    if params["values"].nil?
+      @analyse_data = ArchivalNdsom.fetch_analyse_data
+      if params[:ajax_call].present? && params[:ajax_call] == "true" && params[:isis].present?
+         @analyse_data = @analyse_data.select { |x| x.isin.match?(params[:isis]) }
+      else
+         @analyse_data
+      end
     else
-       @analyse_data
+      @values = params["values"]
     end
     respond_to do |format|
       format.html
       format.js # actually means: if the client ask for js -> return file.js
     end
   end
-
-
 end
